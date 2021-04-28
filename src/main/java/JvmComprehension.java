8ffe90877dd850edea782304509cc86076e4ee57
Alexandr tataritsky <luckonly@gmail.com>
2021-04-28 19:07:46 +0300

    Fixing problems

--- src/main/java/JvmComprehension.java
+++ src/main/java/JvmComprehension.java
@@ -1,18 +1,17 @@
-public class JvmComprehension { /*С помощью classLoader подгрузились system class,
-                                 JvmComprehension.class в metaspace,  инициализация статического
-                                метода printAll, инициализация константы uselessVar = 700*/
+public class JvmComprehension { /*С помощью classLoader подгрузились
+                                    JvmComprehension.class, String.class, Integer.class
+                                    Object.class в metaspace*/
 
-    public static void main(String[] args) {
-        int i = 1;                      // 1 В куче создался объект int со значением 1,  со ссылкой на него  в стеке i
-        Object o = new Object();        // 2 В куче создался объект Object, со ссылкой на него в стеке o
-        Integer ii = 2;                 // 3 В куче создался объект Integer = 2, со ссылкой на него в стеке ii
-        printAll(o, i, ii);             // 4 В стеке был добавлен новый фрейм
-        System.out.println("finished"); // 7 В стеке был добавлен новый фрейм
+    public static void main(String[] args) { //Создается новый фрейм main()
+        int i = 1;                      // 1 В куче создался примитив int со значением 1,  со ссылкой на него во фрейме main()
+        Object o = new Object();        // 2 В куче создался объект типа Object, со ссылкой на него во фрейме main()
+        Integer ii = 2;                 // 3 В куче создался объект типа Integer = 2, со ссылкой на него во фрейме main()
+        printAll(o, i, ii);             // 4 В стеке был добавлен новый фрейм printAll(), внутри фрейма создаются переменные o,i,ii - ссылки на объекты
+        System.out.println("finished"); // 7 В стеке был добавлен новый фрейм println(). В куче создался объект типа String = "finished".
     }
 
     private static void printAll(Object o, int i, Integer ii) {
-        Integer uselessVar = 700;                 /*// 5 Была инициализирована в самом начале как константа,
-                                                        объект Integer = 700 в куче, ссылка в стеке uselessVar */
+        Integer uselessVar = 700;                   // 5 В куче создался объект типа Integer = 700, ссылка в стеке printAll() uselessVar
         System.out.println(o.toString() + i + ii);  // 6 В стеке добавился фрейм toString(), затем println()
     }
 }
